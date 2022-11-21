e3d81c22100bd20b547058a4699bfbbe0d113428
Vedant Bisen <bisenvedant@gmail.com>
2022-11-21 12:22:46 +0530

    .

--- main.py
+++ main.py
@@ -1,54 +1,81 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
-import random
-import time
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import time as tm
 
 class MainWindow(QMainWindow):
     def __init__(self,parent = None):
         super(MainWindow,self).__init__()
         self.setGeometry(1400,100,500,500)
         self.setWindowTitle("Timer")
+        self.setStyleSheet("background-color : rgb(100,100,100)")
         self.initUI()
     
     def initUI(self):
 
-        #Setting Background Color
-        self.Background = QtWidgets.QWidget()
-        self.layout = QtWidgets.QGridLayout()
-        self.setCentralWidget(self.Background)
-        self.Background.setLayout(self.layout)
-        self.Background.setStyleSheet("background-color : rgb(100, 100, 100)")
-        self.Background.setObjectName("BackGround")
+        #Creating a flag
+        self.count = 0
+        self.start = False
 
+        #Creating label
+        self.label = QtWidgets.QLabel(self)
+        self.label.setGeometry(200, 150, 100, 50)
 
-        #Creating a TextBox
-        self.TextBox = QtWidgets.QLineEdit()
-        self.label = QtWidgets.QLabel("Hi")
+        #Show current time button
+        self.showtime_button = QtWidgets.QPushButton(self)
+        self.showtime_button.setGeometry(200, 200, 100, 50)
+        self.showtime_button.setText("Show current time")
+        self.showtime_button.clicked.connect(self.showtime)
 
-        #Creating a button
-        self.button = QtWidgets.QPushButton(self)
-        self.button.setText("Push me!")
+        #Creating a timer button
+        self.timer_button = QtWidgets.QPushButton(self)
+        self.timer_button.setGeometry(200, 250, 100, 50)
+        self.timer_button.setText("Click for timer")
+        self.timer_button.clicked.connect(self.get_seconds)
+
+        #Connecting timer to func
+        timer = QTimer(self)
+        timer.timeout.connect(self.showtime)
+        timer.start(100)
+
+    def showtime(self):
+        if self.start:
+            self.count -=1
+
+            if self.count == 0:
+                self.start = False
+                self.label.setText("Completed")
         
-        #Connecting TextBox to button
-        self.button.clicked.connect(self.timer)
+        if self.start:
+            text = str(self.count / 100)
 
-        #Adding to layout
-        self.layout.addWidget(self.label, 0, 0)
-        self.layout.addWidget(self.TextBox, 1, 0)
-        self.layout.addWidget(self.button, 1, 1)
+            self.label.setText(text)
+        
 
+    def get_seconds(self):
+        self.start = True
 
-    def Textchanged(self):
-        pass
+        seconds, done = QInputDialog.getInt(self, "Seconds", "Enter the number of secs: ")
 
-    def timer(self):
-        s = self.TextBox.text()
-        for i in range(int(s)):
-            i -= 1
-            self.label.setText(str(i))
+        if done:
+            self.count = seconds * 100 
+
+            self.label.setText(str(seconds))
 
 
+
+
+
+
+
+
+    def update(self):
+        self.label.adjustSize()
+
+
+        
 
 
 def main():
